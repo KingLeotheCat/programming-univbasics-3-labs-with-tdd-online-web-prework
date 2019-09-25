3d441ada929788ae2ec3a2a15470799f59734afb
Lenny Lee <lennylee91@gmail.com>
2019-09-25 04:59:03 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
 
 first_number = 1.0
-second_number = 3
+second_number = 3.0
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
